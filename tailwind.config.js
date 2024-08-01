e9f830b676b7e3f41b92ec5859088e0669ef2e93
only1tarunno <tarunno281@gmail.com>
2024-08-01 11:07:32 +0600

    l

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+       screens: {
+      sm: "600px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+     "2xl": "1440px",
+     "3xl": "1536px",          
+     "4xl": "1920px",         
+    },
     },
     plugins: [],
   }
